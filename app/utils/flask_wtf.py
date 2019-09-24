02238205e9ad02b4e78bfb0220dfc18d6c2d8a3e
Luis Henrique Jacinto <henriquejs98@hotmail.com>
2019-09-24 19:20:14 -0300

    Update flask_wtf.py

--- app/utils/flask_wtf.py
+++ app/utils/flask_wtf.py
@@ -4,20 +4,25 @@ from wtforms.validators import DataRequired, InputRequired
 
 class form_usuario(FlaskForm):
     style = {'class':'form-control'}
+    
     nome=StringField("Nome",[
         validators.InputRequired(message="Campo obrigatorio"),
         validators.Length(min=3,max=50,message="O nome deve ter entre 3 e 50 caracteres")],render_kw=style)
+
     senha=PasswordField("Senha",[
         validators.InputRequired(message="Campo obrigatorio"),
         validators.Length(min=3,max=50,message="O nome deve ter entre 3 e 50 caracteres")],render_kw=style)
+
     confirma_senha = PasswordField('Repita a senha',[
         validators.EqualTo("senha",message="As senhas devem ser iguais")],render_kw=style)
+
     email=StringField("Email",[
         validators.InputRequired(message="Campo obrigatorio"),
         validators.Length(min=3,max=50,message="O nome deve ter entre 3 e 50 caracteres"),
         validators.Email(message="Email invalido")],render_kw=style)
-    celular=IntegerField('celular',
-        [validators.InputRequired()],render_kw=style)
+
+    celular=IntegerField('celular',[validators.InputRequired(message="Campo obrigatorio")],render_kw=style)
+
     sexo=SelectField(
         'Sexo',
         choices=[('Masculino', 'Masculino'), ('Feminino', 'Feminino'), ('Não Binario', 'Não Binario')],render_kw=style)
@@ -28,22 +33,29 @@ class form_usuario(FlaskForm):
         else:
             raise ValidationError('sexo invalido')
 
-    nascimento=DateField('Nascimento',[validators.InputRequired()],render_kw=style,format='%Y-%m-%d')
-    cpf=StringField('CPF',[validators.InputRequired()],render_kw=style)
+    nascimento=DateField('Data de nascimento',[validators.InputRequired(message="Campo obrigatorio")],render_kw=style)
+
+    cpf=StringField('CPF',[validators.InputRequired(message="Campo obrigatrio")],render_kw=style)
     def validate_cpf(self,field):
         pass
-    estado=SelectField('Estado',choices=[])##ancap
+    estado=SelectField('Estado',choices=[],render_kw=style)##ancap
     def validate_estado(self,field):
         pass
-    cidade=SelectField('Cidade',choices=[])##ancap
+    cidade=SelectField('Cidade',choices=[],render_kw=style)##ancap
     def validate_cidade(self,field):
         pass
-    cep=StringField("CEP",[validators.InputRequired()],render_kw=style)
-    bairro=StringField("Bairro",[validators.InputRequired()],render_kw=style)
-    rua=StringField("Rua",[validators.InputRequired()],render_kw=style)
-    numero=IntegerField('Numero',[validators.InputRequired()],render_kw=style)
-    complemento=StringField('Complemento',render_kw=style)
-    receber_noticias=BooleanField('Receber noticias')
+
+    cep=StringField("CEP",[validators.InputRequired(message="Campo obrigatorio")],render_kw=style)
+
+    bairro=StringField("Bairro",[validators.InputRequired(message="Campo obrigatorio")],render_kw=style)
+
+    rua=StringField("Rua",[validators.InputRequired(message="Campo obrigatorio")],render_kw=style)
+
+    numero=IntegerField('Numero',[validators.InputRequired(message="Campo obrigatorio")],render_kw=style)
+
+    complemento=StringField("Complemento",render_kw=style)
+
+    receber_noticias=BooleanField("Receber noticias",render_kw=style)
 
     submit = SubmitField('Cadastrar',render_kw={'class':'btn btn-primary'})
 
